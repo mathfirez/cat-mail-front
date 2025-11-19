a8b14ebec596b09f41b32d7775b86e8e2f6493be
mathfirez <mncollacique@gmail.com>
2025-11-19 01:13:46 -0300

    rework

---
+++ scripts.js
@@ -0,0 +1,38 @@
+function send() {
+    const author = document.getElementById("username").value.trim();
+    const content = document.getElementById("content").value.trim();
+	const receiver = "mathfirez";
+
+    if (author === "" || content === "") {
+        alert("All fields must be filled before sending.");
+        return;
+    }
+
+    const message = {
+        Author: author,
+		Receiver: receiver,
+        Content: content
+    };
+
+    fetch("http://srv1137806.hstgr.cloud:8000/message", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(message)
+    })
+    .then(response => {
+        if (response.status === 201) {
+            return response.json();
+        } else {
+            throw new Error("HTTP " + response.status);
+        }
+    })
+    .then(data => {
+        alert("Sent!");
+    })
+    .catch(err => {
+        alert("Unable to send message. Try again later.\nError: " + err);
+        console.error(err);
+    });
+}
